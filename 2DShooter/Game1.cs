ba02d82394ec4fa1a34391321db3da2573141c24
XchgBxBx <xchgbxbx@protonmail.com>
2018-01-04 20:57:57 -0200

    Rocket + Smoke

--- 2DShooter/Game1.cs
+++ 2DShooter/Game1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -35,13 +37,24 @@ namespace _2DShooter
 
         Texture2D carriageTexture;
         Texture2D cannonTexture;
-        
+        Texture2D rocketTexture;
+        Texture2D smokeTexture;
+
         PlayerData[] players;
         int numOfPlayers = 4;
 
         float playersScale;
 
         int currentPlayer = 0;
+
+        bool rocketFlying = false;
+        Vector2 rocketPosition;
+        Vector2 rocketDirection;
+        float rocketAngle;
+        float rocketScaling = 0.1f;
+
+        List<Vector2> smokeList = new List<Vector2>();
+        Random randomizer = new Random();
 
         public Game1()
         {
@@ -87,7 +100,7 @@ namespace _2DShooter
             players[2].Position = new Vector2(486, 433);
             players[3].Position = new Vector2(642, 197);
 
-            playersScale = 70.0f / carriageTexture.Width;
+            playersScale = 50.0f / carriageTexture.Width;
         }
 
         /// <summary>
@@ -119,6 +132,8 @@ namespace _2DShooter
 
             carriageTexture = Content.Load<Texture2D>("carriage");
             cannonTexture = Content.Load<Texture2D>("cannon");
+            rocketTexture = Content.Load<Texture2D>("rocket");
+            smokeTexture = Content.Load<Texture2D>("smoke");
 
             SetUpPlayers();
         }
@@ -162,6 +177,40 @@ namespace _2DShooter
                 players[currentPlayer].Power = 1000;
             if (players[currentPlayer].Power < 0)
                 players[currentPlayer].Power = 0;
+
+            if(kbdState.IsKeyDown(Keys.Enter) || kbdState.IsKeyDown(Keys.Space))
+            {
+                rocketFlying = true;
+
+                rocketPosition = players[currentPlayer].Position;
+                rocketPosition.X += 27;
+                rocketPosition.Y -= 11;
+                rocketAngle = players[currentPlayer].Angle;
+
+                Vector2 up = new Vector2(0, -1);
+                Matrix rotMatrix = Matrix.CreateRotationZ(rocketAngle);
+                rocketDirection = Vector2.Transform(up, rotMatrix);
+                rocketDirection *= players[currentPlayer].Power / 50.0f;
+            }
+        }
+
+        private void UpdateRocket()
+        {
+            if (rocketFlying)
+            {
+                Vector2 gravity = new Vector2(0, 1);
+                rocketDirection += gravity / 10.0f;
+                rocketPosition += rocketDirection;
+                rocketAngle = (float)Math.Atan2(rocketDirection.X, -rocketDirection.Y);
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Vector2 smokePos = rocketPosition;
+                    smokePos.X += randomizer.Next(10) - 5;
+                    smokePos.Y += randomizer.Next(10) - 5;
+                    smokeList.Add(smokePos);
+                }
+            }
         }
 
         /// <summary>
@@ -176,7 +225,17 @@ namespace _2DShooter
 
             ProcessKeyboard();
 
+            UpdateRocket();
+
             base.Update(gameTime);
+        }
+
+        private void DrawText()
+        {
+            PlayerData player = players[currentPlayer];
+            int currentAngle = (int)MathHelper.ToDegrees(player.Angle);
+            spriteBatch.DrawString(font, "Cannon Angle: " + currentAngle, new Vector2(20, 20), player.Color);
+            spriteBatch.DrawString(font, "Cannon Power: " + player.Power, new Vector2(20, 45), player.Color);
         }
 
         private void DrawScene()
@@ -186,14 +245,30 @@ namespace _2DShooter
             spriteBatch.Draw(foregroundTexture, screenRectangle, Color.White);
         }
 
+        private void DrawRocket()
+        {
+            if(rocketFlying)
+            {
+                spriteBatch.Draw(rocketTexture, rocketPosition, null, players[currentPlayer].Color, rocketAngle, new Vector2(42, 240), 0.1f, SpriteEffects.None, 1);
+            }
+        }
+
+        private void DrawSmoke()
+        {
+            foreach(Vector2 smokePos in smokeList)
+            {
+                spriteBatch.Draw(smokeTexture, smokePos, null, Color.White, 0, new Vector2(40, 35), 0.2f, SpriteEffects.None, 1);
+            }
+        }
+
         private void DrawPlayers()
         {
             foreach(PlayerData player in players)
             {
                 if(player.IsAlive)
                 {
-                    int xPos = (int)player.Position.X + 35;
-                    int yPos = (int)player.Position.Y - 15;
+                    int xPos = (int)player.Position.X + 27;
+                    int yPos = (int)player.Position.Y - 11;
                     Vector2 cannonOrigin = new Vector2(11, 50);
 
                     spriteBatch.Draw(cannonTexture, new Vector2(xPos, yPos), null, player.Color, player.Angle, cannonOrigin, playersScale, SpriteEffects.None, 1);
@@ -215,6 +290,9 @@ namespace _2DShooter
 
             DrawScene();
             DrawPlayers();
+            DrawText();
+            DrawRocket();
+            DrawSmoke();
 
             spriteBatch.End();
 
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ﻿# 2DShooter
 
 2D Shooter Game made with MonoGame(https://github.com/MonoGame/MonoGame).
+
 Started following http://www.riemers.net 2D XNA Series.
